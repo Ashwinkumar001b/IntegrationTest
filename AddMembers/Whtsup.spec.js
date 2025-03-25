b9eeb3935bd7b43c0f0ed5d8b07f311d3c8fd582
Ashwinkumar001b <ashwinkumarbaskar9840@gmail.com>
2025-03-25 18:25:02 +0530

    hi

--- AddMembers/Whtsup.spec.js
+++ AddMembers/Whtsup.spec.js
@@ -8,11 +8,12 @@ const userDataDir = "whatsapp-session-new"; // Directory to save session
 function myTest(){
 test("Login to WhatsApp and Save Session", async () => {
    const GroupName = process.env.GROUPNAME
-   const PhnNumber = JSON.parse(process.env.PHONENUMBER)
+   const PhnNumber = process.env.PHONENUMBER
   // const GroupName = "Testing A";
-  // const PhnNumber =["7639002971","7092310772","9940338789"];
-  log("rocess.env.PHONENUMBER", Process.env.PHONENUMBER);
+  // const PhnNumber ="7639002971,7092310772";
+  const phoneArray = PhnNumber.split(',');
   log("GroupName", GroupName);
+  log("phoneArray", phoneArray);
   const browser = await chromium.launchPersistentContext(userDataDir, {
     headless: true, // Open browser visibly
   });
@@ -48,10 +49,10 @@ test("Login to WhatsApp and Save Session", async () => {
       }
     // await page.getByRole("button", { name: Name }).click();
     await page.getByRole("button", { name: "Confirm" }).click();
-    await page
-      .getByRole("dialog")
-      .getByRole("button", { name: "Add member" })
-      .click();
+    // await page
+    //   .getByRole("dialog")
+    //   .getByRole("button", { name: "Add member" })
+    //   .click();
   } catch (error) {
     console.log("⚠️ Login check failed. Please verify manually.");
   }
