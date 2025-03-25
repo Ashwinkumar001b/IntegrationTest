9b3919451522bf4bde7832381bf19c9c0304da51
Ashwinkumar001b <ashwinkumarbaskar9840@gmail.com>
2025-03-25 15:19:31 +0530

    hi

--- AddMembers/Whtsup.spec.js
+++ AddMembers/Whtsup.spec.js
@@ -9,7 +9,7 @@ function myTest(){
 test("Login to WhatsApp and Save Session", async () => {
    const GroupName = process.env.GROUPNAME
    const PhnNumber = process.env.PHONENUMBER
-  // const GroupName = "Test B";
+  // const GroupName = "Test A";
   // const PhnNumber ="7092310772";
   log("PhnNumber", PhnNumber);
   const browser = await chromium.launchPersistentContext(userDataDir, {
