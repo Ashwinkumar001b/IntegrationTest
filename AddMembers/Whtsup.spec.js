c50ecaf786b91d92382274cd28d150c6e0fd32af
Ashwinkumar001b <ashwinkumarbaskar9840@gmail.com>
2025-03-26 13:23:31 +0530

    hi

--- AddMembers/Whtsup.spec.js
+++ AddMembers/Whtsup.spec.js
@@ -2,67 +2,88 @@ import { test, chromium } from "@playwright/test";
 import { log } from "console";
 import { ftruncate } from "fs";
 
-
 const userDataDir = "whatsapp-session-new"; // Directory to save session
 
-function myTest(){
-test("Login to WhatsApp and Save Session", async () => {
-  //  const GroupName = process.env.GROUPNAME
-  //  const PhnNumber = process.env.PHONENUMBER
-  const GroupName = "Testing D";
-  const PhnNumber ="7639002971,8940766936";
-  const phoneNumberArray = PhnNumber.split(',');
-  log("GroupName", GroupName);
-  log("PhnNumber", phoneNumberArray);
+function myTest() {
+  test("Login to WhatsApp and Save Session", async () => {
+     const GroupName = process.env.GROUPNAME
+     const PhnNumber = process.env.PHONENUMBER
+    const integrationType=process.env.TYPE;
+    // // const integrationType = "ADD";
+    // const integrationType = "REMOVE";
+    // const GroupName = "Testing D";
+    // const PhnNumber = "7639002971,8940766936";
+    const phoneNumberArray = PhnNumber.split(",");
 
-  const browser = await chromium.launchPersistentContext(userDataDir, {
-    headless: true, // Open browser visibly
-  });
+    log("GroupName", GroupName);
+    log("PhnNumber", phoneNumberArray);
 
-  const page = await browser.newPage();
-  await page.goto("https://web.whatsapp.com/");
-
-  console.log(" Scan the QR code to log in...");
- // Wait until the WhatsApp chat list loads
-  try {
-    await page.waitForSelector("div[aria-label='Chat list']",{
-      timeout: 120000,
+    const browser = await chromium.launchPersistentContext(userDataDir, {
+      headless: true, // Open browser visibly
     });
-    console.log("✅ Login successful! Session saved.");
-    await page
-      .getByRole("textbox", { name: "Search input textbox" })
-      .fill(GroupName);
-    await page.getByTitle(GroupName, { exact: true }).click();
 
-    await page.getByRole("button", { name: GroupName }).click();
-    await page.getByRole("button", { name: "Add member", exact: true }).click();
-    await page
-      .getByRole("textbox", { name: "Search name or number" })
-      .getByRole("paragraph")
-      .click();
-      for (let number of phoneNumberArray) {
-    await page
-      .getByRole("textbox", { name: "Search name or number" })
-      .fill(number);
-      log("number",number)
-      await page.waitForTimeout(3000);
-    await page.keyboard.press('Enter')
+    const page = await browser.newPage();
+    await page.goto("https://web.whatsapp.com/");
 
+    console.log(" Scan the QR code to log in...");
+    // Wait until the WhatsApp chat list loads
+    try {
+      await page.waitForSelector("div[aria-label='Chat list']", {
+        timeout: 120000,
+      });
+      console.log("✅ Login successful! Session saved.");
+      await page
+        .getByRole("textbox", { name: "Search input textbox" })
+        .fill(GroupName);
+      await page.getByTitle(GroupName, { exact: true }).click();
+
+      await page.getByRole("button", { name: GroupName }).click();
+
+      if (integrationType === "ADD") {
+        await page
+          .getByRole("button", { name: "Add member", exact: true })
+          .click();
+        await page
+          .getByRole("textbox", { name: "Search name or number" })
+          .getByRole("paragraph")
+          .click();
+        for (let number of phoneNumberArray) {
+          await page
+            .getByRole("textbox", { name: "Search name or number" })
+            .fill(number);
+          log("number", number);
+          await page.waitForTimeout(3000);
+          await page.keyboard.press("Enter");
+        }
+        await page.getByRole("button", { name: "Confirm" }).click();
+        await page
+          .getByRole("dialog")
+          .getByRole("button", { name: "Add member" })
+          .click();
+      } else if (integrationType === "REMOVE") {
+        // await page.locator('.x12lumcd.x1n2onr6 .x6ikm8r.x10wlt62.xlyipyv.xuxw1ft').click();
+        // await page.getByRole('button', { name: '1 member' }).nth(1).click();
+        await page.locator('[role="button"]:has-text("member")').nth(1).click();
+
+
+        for (let number of phoneNumberArray) {
+          await page
+          .getByRole("textbox", { name: "Search contacts" })
+          .fill("");
+        await page
+          .getByRole("textbox", { name: "Search contacts" })
+          .fill(number);
+          await page.getByRole('textbox', { name: 'Search contacts' }).press('ArrowDown');
+          await page.keyboard.press("Enter")
+        // await page.getByRole("button", { name: "Remove" }).click();
+        }
       }
-    // await page.getByRole("button", { name: Name }).click();
-    await page.getByRole("button", { name: "Confirm" }).click();
-    await page
-      .getByRole("dialog")
-      .getByRole("button", { name: "Add member" })
-      .click();
-  } catch (error) {
-    console.log("⚠️ Login check failed. Please verify manually.");
-  }
-  await page.pause();
-
-  await browser.close();
-  
-});
+    } catch (error) {
+      console.log("⚠️ Login check failed. Please verify manually.");
+    }
+    await page.pause();
+    await browser.close();
+  });
 }
 
-myTest()
+myTest();
