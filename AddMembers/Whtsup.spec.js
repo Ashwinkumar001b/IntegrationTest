9a55bbdbc627bdfb6e809fc0b2649a1a6ff83c73
Ashwinkumar001b <ashwinkumarbaskar9840@gmail.com>
2025-03-26 15:22:42 +0530

    hiii

--- AddMembers/Whtsup.spec.js
+++ AddMembers/Whtsup.spec.js
@@ -6,13 +6,13 @@ const userDataDir = "whatsapp-session-new"; // Directory to save session
 
 function myTest() {
   test("Login to WhatsApp and Save Session", async () => {
-    //  const GroupName = process.env.GROUPNAME
-    //  const PhnNumber = process.env.PHONENUMBER
-    // const integrationType=process.env.TYPE;
-    // const integrationType = "ADD";
-    const integrationType = "REMOVE";
-    const GroupName = "Testing D";
-    const PhnNumber = "7639002971,8940766936";
+     const GroupName = process.env.GROUPNAME
+     const PhnNumber = process.env.PHONENUMBER
+    const integrationType=process.env.TYPE;
+    // // const integrationType = "ADD";
+    // const integrationType = "REMOVE";
+    // const GroupName = "Testing D";
+    // const PhnNumber = "7639002971";
     const phoneNumberArray = PhnNumber.split(",");
 
     log("GroupName", GroupName);
