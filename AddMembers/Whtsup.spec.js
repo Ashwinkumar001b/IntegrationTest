eaddbdc9559eb1642573d36b1933dc52e6d899e1
Ashwinkumar001b <ashwinkumarbaskar9840@gmail.com>
2025-03-28 18:49:55 +0530

    Auto commit after test run

--- AddMembers/Whtsup.spec.js
+++ AddMembers/Whtsup.spec.js
@@ -11,7 +11,7 @@ function myTest() {
     // const integrationType = process.env.TYPE;
     const integrationType = "ADD";
     // const integrationType = "REMOVE";
-    const GroupName = "Testing A";
+    const GroupName = "Testing B";
     // const PhnNumber = "7639002971,8940766936,9600392639";
     const PhnNumber = "8940766936";
 
