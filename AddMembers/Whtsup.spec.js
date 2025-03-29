2f3c06c8a2dd01a358309892bc51a2e54ac20037
Ashwinkumar001b <ashwinkumarbaskar9840@gmail.com>
2025-03-29 21:38:40 +0530

    hiee

--- AddMembers/Whtsup.spec.js
+++ AddMembers/Whtsup.spec.js
@@ -11,15 +11,15 @@ function myTest() {
     const integrationType = process.env.TYPE;
     // const integrationType = "ADD";
     // // const integrationType = "REMOVE";
-    // const GroupName = "Testing C";
+    // const GroupName = "Testing F";
     // // const PhnNumber = "7639002971,8940766936,9600392639";
-    // const PhnNumber = "9600392639";
+    // const PhnNumber = "9940338789";
 
-  const phoneNumberArray = PhnNumber.split(",");
+    const phoneNumberArray = PhnNumber.split(",");
 
     log("GroupName", GroupName);
     log("PhnNumber", phoneNumberArray);
-    log("integrationType", integrationType); 
+    log("integrationType", integrationType);
 
     const browser = await chromium.launchPersistentContext(userDataDir, {
       headless: true, // Open browser visibly
@@ -38,106 +38,139 @@ function myTest() {
       await page
         .getByRole("textbox", { name: "Search input textbox" })
         .fill(GroupName);
-        await page.waitForTimeout(2000);
-        await page.keyboard.press("Enter");
+      await page.waitForTimeout(3000);
 
-      // await page.getByText(GroupName, { exact: true }).nth(1).click();
-      await page.waitForTimeout(2000);
+      const secondChatsLocator = page
+        .locator('div[aria-label="Search results."]')
+        .locator('div:has-text("Chats")')
+        .nth(1);
 
-      await page.getByRole("button", { name: `${GroupName}` }).click();
+      await page.waitForTimeout(1000);
 
+      const isChatsHidden = await secondChatsLocator.isHidden();
 
-      if (integrationType == "ADD") {
+      if (isChatsHidden) {
         await page
-          .getByRole("button", { name: "Add member", exact: true })
+          .locator("span")
+          .filter({ hasText: "new-chat-outlinemenu" })
+          .getByLabel("Menu")
           .click();
+        await page.getByRole("button", { name: "New group" }).click();
+        for (let number of phoneNumberArray) {
+          await page.getByPlaceholder(" ").click();
+          await page.getByPlaceholder(" ").fill(number);
+          await page.waitForTimeout(1000);
+          await page.keyboard.press("Enter");
+        }
+
+        await page.getByRole("button", { name: "Next" }).click();
+        await page
+          .getByRole("textbox", { name: "Group subject (optional)" })
+          .fill(GroupName);
+        await page.getByRole("button", { name: "Create group" }).click();
+      } else {
+        await page.keyboard.press("Enter");
+        await page.waitForTimeout(2000);
+
+        await page.getByRole("button", { name: `${GroupName}` }).click();
+
+        if (integrationType == "ADD") {
+          await page
+            .getByRole("button", { name: "Add member", exact: true })
+            .click();
           await page.waitForTimeout(2000);
 
-        await page
-          .getByRole("textbox", { name: "Search name or number" })
-          .getByRole("paragraph")
-          .click();
-        for (let number of phoneNumberArray) {
           await page
             .getByRole("textbox", { name: "Search name or number" })
-            .fill(number);
-          await page.waitForTimeout(1000);
+            .getByRole("paragraph")
+            .click();
+          for (let number of phoneNumberArray) {
+            await page
+              .getByRole("textbox", { name: "Search name or number" })
+              .fill(number);
+            await page.waitForTimeout(1000);
 
-          log("number", number);
-          const alreadyInTheGroup = await page.locator(
-            "text=Already added to group"
-          );
+            log("number", number);
+            const alreadyInTheGroup = await page.locator(
+              "text=Already added to group"
+            );
 
-          const noContactAdd = await page.locator(
-            "text=No chats, contacts or messages found"
-          );
-          await page.waitForTimeout(2000);
+            const noContactAdd = await page.locator(
+              "text=No chats, contacts or messages found"
+            );
+            await page.waitForTimeout(2000);
 
-          if (
-            (await alreadyInTheGroup.isHidden()) &&
-            (await noContactAdd.isHidden())
-          ) {
-            // await page.waitForTimeout(2000);
-            await page.keyboard.press("Enter");
+            if (
+              (await alreadyInTheGroup.isHidden()) &&
+              (await noContactAdd.isHidden())
+            ) {
+              // await page.waitForTimeout(2000);
+              await page.keyboard.press("Enter");
+            }
           }
-        }
-        await page.getByRole("button", { name: "Confirm" }).click();
-        await page
-          .getByRole("dialog")
-          .getByRole("button", { name: "Add member" })
-          .click();
-        await page.waitForTimeout(4000);
-        const invitePeople = await page.getByRole("button", {
-          name: "Invite to group",
-          exact: true,
-        });
-        if (await invitePeople.isVisible()) {
-          console.log("Invite button is visible, clicking...");
-          await invitePeople.click();
-          
-          // Wait for the "Next" button to appear and be visible
-          const nextButton = await page.locator('[data-icon="send"]');
-          // const nextButton = await page.locator('[aria-label="Next"]');
-          await nextButton.waitFor({ state: 'visible', timeout: 6000 });
-        
-          if (await nextButton.isEnabled()) {
-            console.log("Next button is visible and enabled, clicking...");
-            await nextButton.click();
-            // await page.waitForTimeout(2000); // Wait for the action to complete
-            console.log("Invite sent successfully");
-          } else {
-            console.log("Next button is not enabled or visible, retrying...");
-          }
-        } else {
-          console.log("Invite button is not visible, skipping...");
-        }
-        
-        await page.waitForTimeout(1000);
-  
-        log("Added successfully");
-        //
-        //
-      } else if (integrationType == "REMOVE") {
-        await page.locator('[role="button"]:has-text("member")').nth(1).click();
-        for (let number of phoneNumberArray) {
-          await page.getByRole("textbox", { name: "Search contacts" }).fill("");
+          await page.getByRole("button", { name: "Confirm" }).click();
           await page
-            .getByRole("textbox", { name: "Search contacts" })
-            .fill(number);
+            .getByRole("dialog")
+            .getByRole("button", { name: "Add member" })
+            .click();
+          await page.waitForTimeout(4000);
+          const invitePeople = await page.getByRole("button", {
+            name: "Invite to group",
+            exact: true,
+          });
+          if (await invitePeople.isVisible()) {
+            console.log("Invite button is visible, clicking...");
+            await invitePeople.click();
+
+            // Wait for the "Next" button to appear and be visible
+            const nextButton = await page.locator('[data-icon="send"]');
+            // const nextButton = await page.locator('[aria-label="Next"]');
+            await nextButton.waitFor({ state: "visible", timeout: 6000 });
+
+            if (await nextButton.isEnabled()) {
+              console.log("Next button is visible and enabled, clicking...");
+              await nextButton.click();
+              // await page.waitForTimeout(2000); // Wait for the action to complete
+              console.log("Invite sent successfully");
+            } else {
+              console.log("Next button is not enabled or visible, retrying...");
+            }
+          } else {
+            console.log("Invite button is not visible, skipping...");
+          }
+
           await page.waitForTimeout(1000);
 
-          const noContact = await page.locator("text=No contacts found");
-
-          if (await noContact.isHidden()) {
+          log("Added successfully");
+          //
+          //
+        } else if (integrationType == "REMOVE") {
+          await page
+            .locator('[role="button"]:has-text("member")')
+            .nth(1)
+            .click();
+          for (let number of phoneNumberArray) {
             await page
               .getByRole("textbox", { name: "Search contacts" })
-              .press("ArrowDown");
+              .fill("");
+            await page
+              .getByRole("textbox", { name: "Search contacts" })
+              .fill(number);
+            await page.waitForTimeout(1000);
 
-            await page.keyboard.press("Enter");
-            await page.getByRole("button", { name: "Remove" }).click();
+            const noContact = await page.locator("text=No contacts found");
+
+            if (await noContact.isHidden()) {
+              await page
+                .getByRole("textbox", { name: "Search contacts" })
+                .press("ArrowDown");
+
+              await page.keyboard.press("Enter");
+              await page.getByRole("button", { name: "Remove" }).click();
+            }
           }
+          log("Removed successfully");
         }
-        log("Removed successfully");
       }
     } catch (error) {
       console.log("⚠️ Login check failed. Please verify manually.");
@@ -145,18 +178,16 @@ function myTest() {
     // await page.pause();
     await browser.close();
 
-    try {
-      console.log("Pushing code changes to GitHub...");
-      execSync('git add .'); // Stage all changes
-      execSync('git commit -m "Auto commit after test run"'); // Commit changes with a message
-      execSync('git push origin main'); // Push to the main branch (change 'main' if you're using another branch)
-      console.log("✅ Code pushed to GitHub successfully!");
-    } catch (gitError) {
-      console.error("⚠️ Failed to push code to GitHub:", gitError.message);
-    }
+    // try {
+    //   console.log("Pushing code changes to GitHub...");
+    //   execSync('git add .'); // Stage all changes
+    //   execSync('git commit -m "Auto commit after test run"'); // Commit changes with a message
+    //   execSync('git push origin main'); // Push to the main branch (change 'main' if you're using another branch)
+    //   console.log("✅ Code pushed to GitHub successfully!");
+    // } catch (gitError) {
+    //   console.error("⚠️ Failed to push code to GitHub:", gitError.message);
+    // }
   });
 }
 
 myTest();
-
-
